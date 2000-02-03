0e952f758ccc3ccfd12f7917710d638d14c890de
Ed Luke <edluke@gmail.com>
2000-02-03 05:03:43 +0000

    Added support for floating point exeception handling on sun SPARC's

--- src/Tools/fpe.cc
+++ src/Tools/fpe.cc
@@ -1,27 +1,41 @@
 #include <Tools/fpe.h>
 
-namespace Loci {
 
-#ifdef NO_FPE_EXCEPTIONS
+#ifdef SPARC
 
-void set_fpe_abort()
-{
+extern "C" {
+  extern void SPARC_set_fpe_abort() ;
 }
 
+namespace Loci {
+  void set_fpe_abort() {
+    SPARC_set_fpe_abort() ;
+  }
+}
 #else
+
+#ifdef SGI
+
 #include <stdio.h>
 #include <sigfpe.h>
-//#include <signal.h>
 
-void set_fpe_abort()
-{
+namespace Loci {
+  void set_fpe_abort() {
     sigfpe_[_DIVZERO].abort = 1 ;
     sigfpe_[_OVERFL].abort = 1 ;
     sigfpe_[_INVALID].abort = 1 ;
     handle_sigfpes(_DEBUG,_EN_OVERFL|_EN_DIVZERO|_EN_INVALID,
                    0,_ABORT_ON_ERROR,0) ;
+  }
 }
-
+                      
+#else
+namespace Loci {
+  
+  void set_fpe_abort()
+    {
+    }
+}
+#endif
 #endif
 
-}
