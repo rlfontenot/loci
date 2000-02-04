0bdbf192c72666ee9fd34d3f1d32ad3ac3c0a9d3
Ed Luke <edluke@gmail.com>
2000-02-04 00:14:13 +0000

    Added fpe exception handlers for linux

--- src/Tools/fpe.cc
+++ src/Tools/fpe.cc
@@ -30,12 +30,61 @@ namespace Loci {
 }
                       
 #else
+#ifdef LINUX_X86
+
+#include <stdlib.h>
+#include <fenvwm.h>
+#include <iostream>
+using std::cerr ;
+using std::endl ;
+
+namespace Loci {
+  int report_overflow(struct sigcontext_struct *sig,
+                      struct i387_hard_struct *fpu)
+  {
+    cerr << "FPU Overflow exception occured at "
+         << (void *) fpu->fip << endl ;
+    abort() ;
+    return 0 ;
+  }
+  int report_divzero(struct sigcontext_struct *sig,
+                      struct i387_hard_struct *fpu)
+  {
+    cerr << "FPU Divide by Zero exception occured at "
+         << (void *) fpu->fip << endl ;
+    abort() ;
+    return 0 ;
+  }
+  int report_invalid(struct sigcontext_struct *sig,
+                     struct i387_hard_struct *fpu)
+  {
+    cerr << "FPU INVALID operation exception occured at "
+         << (void *) fpu->fip << endl ;
+    abort() ;
+    return 0 ;
+  }
+
+  ftrap_t overflow = (ftrap_t) &report_overflow;
+  ftrap_t divzero  = (ftrap_t) &report_divzero ;
+  ftrap_t invalid  = (ftrap_t)&report_invalid ;
+  
+  void set_fpe_abort() {
+    fesetvector(&overflow,FE_OVERFLOW) ;
+    feenabletraps(FE_OVERFLOW) ;
+    fesetvector(&divzero,FE_DIVBYZERO) ;
+    feenabletraps(FE_DIVBYZERO) ;
+    fesetvector(&invalid,FE_INVALID) ;
+    feenabletraps(FE_INVALID) ;
+  }
+}  
+#else
 namespace Loci {
   
   void set_fpe_abort()
     {
     }
 }
+#endif
 #endif
 #endif
 
