ff21ff095c93f410b0e040f5f8ac442bbb73b470
Zhaohui Huo <>
2000-08-24 14:39:11 +0000

    commit constraint.cc

--- src/System/constraint.cc
+++ src/System/constraint.cc
@@ -43,6 +43,52 @@ ostream &constraintRep::Print(ostream &s) const {
     return s ;
 }
 
+void constraintRep::readhdf5( H5::Group group){
+  try{
+     //get constraint data
+     H5::DataSet dataset_constraint = group.openDataSet( "constraint");
+     H5::DataSpace dataspace_constraint = dataset_constraint.getSpace();
+     hsize_t dims_constraint[1];
+     dataspace_constraint.getSimpleExtentDims( dims_constraint, NULL);
+     int *data_constraint = new int[dims_constraint[0]];
+     dataset_constraint.read( data_constraint, H5::PredType::NATIVE_INT );
+     for(int i=0;i<dims_constraint[0];i++){
+       constraint |=interval(data_constraint[i],data_constraint[i+1]);
+       i++;
+     }
+     delete [] data_constraint;
+  }
+  catch( H5::HDF5DatasetInterfaceException error ){error.printerror();}
+  catch( H5::HDF5DataspaceInterfaceException error ){error.printerror();}
+  catch( H5::HDF5DatatypeInterfaceException error ){error.printerror();}
+}
+
+void constraintRep::writehdf5( H5::Group group,entitySet& en) const{
+  hsize_t dimf_constraint[1];
+   int RANK=1;
+    
+    int num_intervals=constraint.num_intervals();
+    dimf_constraint[0]=num_intervals*2;
+    interval *it = new interval[num_intervals];
+    int *data_constraint = new int[num_intervals*2];//get the constraint data
+    for(int i=0;i<num_intervals;i++){
+      it[i]=constraint[i];
+      data_constraint[i*2]=it[i].first;
+      data_constraint[i*2+1]=it[i].second;
+    }
+    try{
+      //write constraint
+      H5::DataSpace dataspace_constraint( RANK, dimf_constraint );
+      H5::DataSet dataset_constraint = group.createDataSet( "constraint", H5::PredType::NATIVE_INT, dataspace_constraint );
+      dataset_constraint.write( data_constraint, H5::PredType::NATIVE_INT );
+    }
+    catch( H5::HDF5DatasetInterfaceException error ){error.printerror();}
+    catch( H5::HDF5DataspaceInterfaceException error ){error.printerror();}
+    catch( H5::HDF5DatatypeInterfaceException error ){error.printerror();}
+    
+    delete [] it;
+    delete [] data_constraint;
+}
 
 istream &constraintRep::Input(istream &s) {
     entitySet e ;
