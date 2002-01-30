524e408b9fc7e11e4896aff59fd34f96d8cc72df
Thomas George <>
2002-01-30 04:26:24 +0000

    broke up the parametric_rdb routine  into two . rename_rule and parametric_rdb

--- src/System/param_rule.cc
+++ src/System/param_rule.cc
@@ -38,9 +38,86 @@ rule_db local_modify_time_vars(rule_db& rdb, const std::string &sn) {
   }
   return new_rdb ;
 }
+  rule rename_rule(rule r, std::map<variable, variable> &vm) { 
+  std::vector<string> str_vec ;//This is to deal with the renaming
+  //of other variables eg. W_f, f_W etc
+  std::set<string> ren_tars ; // Create a set  of names of
+  // renamed target variables  
   
+  variableSet source = r.sources() ;
+  for(variableSet::const_iterator tvsi = source.begin(); tvsi != source.end(); ++tvsi ) { 
+    string name = variable(*tvsi).get_info().name ;
+    if(((name.find("_")) != string::npos) && (!variable(*tvsi).get_arg_list().size()))
+      str_vec.push_back(name) ;
+  }
+  
+  variableSet target = r.targets() ;
+  for(variableSet::const_iterator tvsi = target.begin(); tvsi
+	!= target.end(); ++tvsi ) { 
+    variable var = variable(*tvsi) ;
+    std::vector<int> tvint = var.get_arg_list() ; 
+    for(std::vector<int>::const_iterator tvi = tvint.begin(); tvi != tvint.end(); ++tvi) 
+      ren_tars.insert(variable(*tvi).get_info().name) ;
+  }
+  
+  
+  for(std::vector<string>::const_iterator svs =
+	str_vec.begin(); svs != str_vec.end(); ++svs) {
+    string name = *svs ;
+    string orig = name ;
+    string replace ;
+    for(std::set<string>::const_iterator ssi =
+	  ren_tars.begin(); ssi != ren_tars.end(); ++ssi)
+      {
+	int i ;
+	variable s = variable(*ssi) ; 
+	do {
+	  i = name.find(*ssi) ;
+	  if(i != string::npos)  {
+	    replace = vm[s].get_info().name ;
+	    name.erase(i, i+(*ssi).size()) ;
+	    name.insert(i, replace) ;
+	  }
+	}while(i != string::npos) ;
+	
+	std::vector<int> rep = vm[s].get_arg_list() ;
+	if(rep.size()) {
+	  string par ;
+	  int p_open = 0 ;
+	  par.append("(") ;
+	  p_open++ ;
+	  std::vector<int> trep = rep ;
+	  while(trep.size()) {
+	    trep.clear() ;
+	    for(std::vector<int>::const_iterator pi = rep.begin() ; pi != rep.end(); ++pi) {
+	      trep = variable(*pi).get_arg_list() ;
+	      par.append("(") ;
+	      par.append(variable(*pi).get_info().name) ;
+	      p_open++ ;
+	    }
+	    rep = trep ;
+	  }
+	  
+	  while(p_open) {
+	    par.append(")") ;
+	    p_open-- ;
+	  }
+	  
+	  name.append(par) ;
+	}
+      }
+    if(name != orig)
+      vm[variable(orig)] = variable(name) ;
+  }
+  rule_implP rp = r.get_rule_implP() ;
+  rp->rename_vars(vm) ;
+  
+  return(rule(rp)) ;
+}
+
+
 rule_db parametric_rdb(rule_db& rdb) {
-   Loci::rule_db par_rdb ;
+  Loci::rule_db par_rdb ;
   ruleSet param_target, param_source, use_param_rule, added_rules ;
   variableSet source, target ;
   std::map<variable, ruleSet> mruleset ; // This data structure is
@@ -109,24 +186,6 @@ rule_db parametric_rdb(rule_db& rdb) {
       }
     }
   }
-  
-  //Remove the rules which uses the parametric rules(use_param_rule)
-  //from the param_source ruleSet. The remaining ruleSet will have the 
-  //parametric rules with variables(that could be parametric) in the source.
-  param_source -= use_param_rule ;
-  for(ruleSet::const_iterator rsi = param_source.begin(); rsi != param_source.end(); ++rsi) { 
-    target = rsi->targets() ;
-    for(variableSet::const_iterator vsi = target.begin(); vsi !=
-	  target.end(); ++vsi) { 
-      vint = variable(*vsi).get_arg_list() ;
-      if(vint.size()) {
-	variable tmp = variable(*vsi) ;
-	mvarset[tmp.get_info().name] += *vsi ;
-	mruleset[tmp] += *rsi ;
-      }
-    }
-  }
-  variableSet newvars ;
   //This part handles the recursive parametric rules. 
   ruleSet wrule, nrule ;
   if(param_vars != EMPTY) {
@@ -161,12 +220,11 @@ rule_db parametric_rdb(rule_db& rdb) {
 		      if(variable(*tvsi).get_arg_list().size()) {
 			if(mvarset.find(variable(*tvsi).get_info().name) != mvarset.end())
 			  if(!mvarset.find(variable(*tvsi).get_info().name)->second.inSet(*tvsi)) {
-			    newvars += *vci ;
+			    param_vars += *vci ;
 			    std::vector<int> vin = variable(*tvsi).get_arg_list() ;
 			    if(vin.size()) 
-			      if(!variable(*(vin.begin())).get_arg_list().size()) {
-				newvars += *tvsi ;
-			      }	
+			      if(!variable(*(vin.begin())).get_arg_list().size()) 
+				param_vars += *tvsi ;
 			  }
 		      }
 		  }
@@ -179,7 +237,6 @@ rule_db parametric_rdb(rule_db& rdb) {
       }
     }
   }
-  param_vars += newvars ;
   
   //Add the non parametric rules to the rule database.  
   for(ruleSet::const_iterator rsi = rset.begin(); rsi != rset.end(); ++rsi) {
@@ -187,12 +244,11 @@ rule_db parametric_rdb(rule_db& rdb) {
     par_rdb.add_rule(rule(rp)) ;
     added_rules += rule(rp) ;
   }
+  
   variableSet working, newset ;
   working = param_vars;
   newset = param_vars ;
   while(newset != EMPTY) {
-    std::vector<string> str_vec ;//This is to deal with the renaming
-    //of other variables eg. W_f, f_W etc
     newset = EMPTY ;
     for(variableSet::const_iterator vsi = working.begin(); vsi != working.end(); ++vsi) {
       std::map<variable, variable> vm ;
@@ -201,8 +257,7 @@ rule_db parametric_rdb(rule_db& rdb) {
       for(variableSet::const_iterator mvsi = vs.begin(); mvsi != vs.end(); ++mvsi)
 	if(v.get_arg_list().size() == variable(*mvsi).get_arg_list().size()) {
 	  ruleSet rs = mruleset.find(variable(*mvsi))->second ;
-	  std::set<string> ren_tars ; // Create a set  of names of
-	  // renamed target variables  
+	
 	  for(ruleSet::const_iterator rsi = rs.begin(); rsi != rs.end(); ++rsi ) {
 	    target = rsi->targets() ;
 	    for(variableSet::const_iterator tvsi = target.begin(); tvsi
@@ -219,11 +274,11 @@ rule_db parametric_rdb(rule_db& rdb) {
 		if(vm.find(variable(*tvi)) == vm.end()) {
 		  vm[variable(*tvi)] = variable(*vi) ;
 		}
-		ren_tars.insert(variable(*tvi).get_info().name) ;
 	      }
 	    }
+	    
 	    source = rsi->sources() ;
-	    for(variableSet::const_iterator tvsi = source.begin(); tvsi != source.end(); ++tvsi ) { 
+	    for(variableSet::const_iterator tvsi = source.begin(); tvsi != source.end(); ++tvsi ) {    
 	      std::vector<int> tmp_vint = variable(*tvsi).get_arg_list() ;
 	      std::vector<int> tmp_vec, vec_int ;
 	      vec_int = tmp_vint ;
@@ -284,62 +339,10 @@ rule_db parametric_rdb(rule_db& rdb) {
 		  variable(variable(*tvsi), vec) ;
 		newset +=  variable(variable(*tvsi), vec) ;
 	      }
-	      string name = variable(*tvsi).get_info().name ;
-	      if(((name.find("_")) != string::npos) && (!variable(*tvsi).get_arg_list().size()))
-		str_vec.push_back(name) ;
-	    }
 	    
-	    for(std::vector<string>::const_iterator svs =
-		  str_vec.begin(); svs != str_vec.end(); ++svs) {
-	      string name = *svs ;
-	      string orig = name ;
-	      string replace ;
-	      for(std::set<string>::const_iterator ssi =
-		    ren_tars.begin(); ssi != ren_tars.end(); ++ssi)
-		{
-		  int i ;
-		  variable s = variable(*ssi) ; 
-		  do {
-		    i = name.find(*ssi) ;
-		    if(i != string::npos)  {
-		      replace = vm[s].get_info().name ;
-		      name.erase(i, i+(*ssi).size()) ;
-		      name.insert(i, replace) ;
-		    }
-		  }while(i != string::npos) ;
-		  
-		  std::vector<int> rep = vm[s].get_arg_list() ;
-		  if(rep.size()) {
-		    string par ;
-		    int p_open = 0 ;
-		    par.append("(") ;
-		    p_open++ ;
-		    std::vector<int> trep = rep ;
-		    while(trep.size()) {
-		      trep.clear() ;
-		      for(std::vector<int>::const_iterator pi = rep.begin() ; pi != rep.end(); ++pi) {
-			trep = variable(*pi).get_arg_list() ;
-			par.append("(") ;
-			par.append(variable(*pi).get_info().name) ;
-			p_open++ ;
-		      }
-		      rep = trep ;
-		    }
-		    
-		    while(p_open) {
-		      par.append(")") ;
-		      p_open-- ;
-		    }
-		    
-		    name.append(par) ;
-		  }
-		}
-	      if(name != orig)
-		vm[variable(orig)] = variable(name) ;
 	    }
-	    
-	    rule_implP rp = rsi->get_rule_implP() ;
-	    rp->rename_vars(vm) ;
+	    rule r = rename_rule(*rsi,vm) ;
+	    rule_implP rp = r.get_rule_implP() ;
 	    if(!added_rules.inSet(rule(rp))) {
 	      par_rdb.add_rule(rule(rp)) ;
 	      cout << "Adding rule " << rule(rp) << endl ;
@@ -351,8 +354,8 @@ rule_db parametric_rdb(rule_db& rdb) {
     
     working = newset ;
   }
+  
   return par_rdb ;
-  
-  
 }
+
 }
