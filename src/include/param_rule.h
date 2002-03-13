37856e9993534d6ba33007c2e8d365c2a01693d7
Ed Luke <edluke@gmail.com>
2002-03-13 18:50:38 +0000

    Added time editing for rules

--- src/include/param_rule.h
+++ src/include/param_rule.h
@@ -1,7 +1,7 @@
 #include <rule.h>
 
 namespace Loci {
-  rule_db local_modify_time_vars(rule_db& rdb, const std::string &sn) ;
+  rule prepend_time_level(rule r, string prepend) ;
   rule rename_rule(rule r, std::map<variable, variable> &vm) ; 
   rule_db parametric_rdb(rule_db &rdb,variableSet query_vars) ;
 }
