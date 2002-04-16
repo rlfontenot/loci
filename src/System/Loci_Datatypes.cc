a733dfd5b9d3d436db831128a8df989b24851f8b
Verma <>
2002-04-16 19:28:02 +0000

    All C++ basic datatypes implemented

--- src/System/Loci_Datatypes.cc
+++ src/System/Loci_Datatypes.cc
@@ -8,16 +8,33 @@ using std::endl ;
 namespace Loci {
   hid_t   AtomicType::get_hdf5_type() const {
     switch( atom ) {
+    case BOOL:
+      return (H5Tcopy(H5T_NATIVE_HBOOL));
     case FLOAT:
       return (H5Tcopy(H5T_NATIVE_FLOAT));
     case DOUBLE:
       return (H5Tcopy(H5T_NATIVE_DOUBLE));
+    case LONG_DOUBLE:
+      return (H5Tcopy(H5T_NATIVE_LDOUBLE));
     case INT:
       return (H5Tcopy(H5T_NATIVE_INT));
+    case UNSIGNED:
+      return (H5Tcopy(H5T_NATIVE_UINT));
     case CHAR:
       return (H5Tcopy(H5T_NATIVE_CHAR));
+    case UNSIGNED_CHAR:
+      return (H5Tcopy(H5T_NATIVE_UCHAR));
+    case SHORT:
+      return (H5Tcopy(H5T_NATIVE_SHORT));
+    case UNSIGNED_SHORT:
+      return (H5Tcopy(H5T_NATIVE_USHORT));
+    case LONG:
+      return (H5Tcopy(H5T_NATIVE_LONG));
+    case UNSIGNED_LONG:
+      return (H5Tcopy(H5T_NATIVE_ULONG));
     default:
-      cerr << "Unknown type" << endl;
+      cerr << "Unknown Basic datatype  " << atom << endl;
+      abort();
     }
     return 0;
   }
@@ -32,16 +49,15 @@ namespace Loci {
 
     for(int k = 0; k < rank; k++)
       array_dims[k]  = dimension[k];
+
 #ifdef HDF5V1_2
     hid_t vDatatype  = H5Tcreate( H5T_COMPOUND, numBytes);
-    H5Tinsert_array(vDatatype, "Array", 0, 
-                    rank, array_dims, NULL, hdf5T);
+    H5Tinsert_array(vDatatype, "Array", 0, rank, array_dims, NULL, hdf5T);
     return vDatatype ;
 #else
     return   H5Tarray_create(hdf5T,rank,array_dims,NULL) ;
 #endif
     }
-
 
 
   hid_t CompoundType::get_hdf5_type() const
