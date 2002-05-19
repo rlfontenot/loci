2166c3f47b9ad33e589e016460859f7ba693edbe
Ed Luke <edluke@gmail.com>
2002-05-19 19:23:33 +0000

    Moved the hdf5_readwrite inline functions to non-inline versions. This should help with compile times.

--- src/System/hdf5_readwrite.cc
+++ src/System/hdf5_readwrite.cc
@@ -1,61 +1,93 @@
-#include <entitySet.h>
+#include <hdf5_readwrite.h>
 
-#include <data_traits.h>
 
 namespace Loci {
   
   void HDF5_WriteDomain(hid_t group_id, const entitySet &en )
   {
 
-    hsize_t dimension[1];
+    hsize_t dimension;
 
     int rank = 1;     
     int num_intervals = en.num_intervals(); 
 
-    dimension[0] = num_intervals*2;        //  size of 1D Array 
+    if( num_intervals < 1) return;
+
+    dimension = num_intervals*2;        //  size of 1D Array 
     interval *it = new interval[num_intervals];
 
     int *data = new int[num_intervals*2];      
 
     for(int i=0;i<num_intervals;i++){
-      it[i]=en[i];
+      it[i]       = en[i];
       data[i*2]   = it[i].first;
       data[i*2+1] = it[i].second;
     }
 
-    hid_t dataspace = H5Screate_simple(rank, dimension, NULL);
-    hid_t datatype  = H5Tcopy(H5T_NATIVE_INT);
-    hid_t dataset   = H5Dcreate(group_id, "Domain", datatype, dataspace, H5P_DEFAULT);
-    H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+    hid_t dataspace = H5Screate_simple(rank, &dimension, NULL);
+    hid_t datatype  = H5T_NATIVE_INT;
+    hid_t dataset   = H5Dcreate(group_id, "Interval Set", datatype, dataspace, H5P_DEFAULT);
+    H5Dwrite(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
 
+    delete [] data;
+    delete [] it;
+    H5Sclose(dataspace);
+    H5Dclose(dataset);
   }
 
-  //**************************************************************************
-
-  void HDF5_ReadDomain( hid_t group_id, entitySet &eset)
+  void HDF5_Local2Global( hid_t group_id, entitySet &eset, Map &l_g)
   {
+    int        indx = 0;
+    hsize_t    dimension;
+    hid_t      dataset, dataspace;
 
-    hsize_t    dimension[1];
+    dataset    = H5Dopen(group_id, "Map");
+    dataspace  = H5Dget_space(dataset);
 
-    hid_t dataset   = H5Dopen( group_id, "Domain");
-    hid_t dataspace = H5Dget_space(dataset);
-    H5Sget_simple_extent_dims (dataspace, dimension, NULL);
+    H5Sget_simple_extent_dims (dataspace, &dimension, NULL);
 
-    int *data = new int[dimension[0]];
-    H5Dread (dataset, H5T_NATIVE_INT, H5S_ALL, dataspace,
-                      H5P_DEFAULT, data);
+    int *data = new int[dimension];
 
-    for(int i=0;i< dimension[0];i++){
-        eset |= interval(data[i],data[i+1]);
-        i++;
-    }
+    H5Dread( dataset, H5T_NATIVE_INT, H5S_ALL, dataspace,
+                H5P_DEFAULT, data);
+
+    l_g.allocate(eset);
+    entitySet :: const_iterator ci;
+    for( ci = eset.begin(); ci != eset.end(); ++ci)
+         l_g[*ci] = data[indx++];
 
     delete [] data;
     H5Sclose(dataspace);
     H5Dclose(dataset);
   }
 
-  //*************************************************************************
+
+
+  void HDF5_ReadDomain( hid_t group_id, entitySet &eset)
+  {
+    hsize_t    dimension;
+    hid_t      dataset, dataspace;
+
+    dataset    = H5Dopen(group_id, "Interval Set");
+    dataspace  = H5Dget_space(dataset);
+
+    H5Sget_simple_extent_dims (dataspace, &dimension, NULL);
+
+    int *data = new int[dimension];
+
+    H5Dread( dataset, H5T_NATIVE_INT, H5S_ALL, dataspace,
+                H5P_DEFAULT, data);
+
+    eset = EMPTY;
+    for(int i=0;i< dimension;i++){
+        eset |= interval(data[i],data[i+1]);
+        i++;
+    }
+    delete [] data;
+    H5Sclose(dataspace);
+    H5Dclose(dataset);
+  }
+
 
   void HDF5_WriteVecSize(hid_t group_id, const int &size )
   {
@@ -64,36 +96,32 @@ namespace Loci {
     int     rank = 1;
 
     hid_t vDataspace = H5Screate_simple(rank, &dimension, NULL);
-    hid_t vDatatype  = H5Tcopy(H5T_NATIVE_INT);
+    hid_t vDatatype  = H5T_NATIVE_INT;
     hid_t vDataset   = H5Dcreate(group_id, "VecSize", vDatatype, vDataspace,
                                  H5P_DEFAULT);
+
     H5Dwrite(vDataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &size);
 
     H5Sclose( vDataspace );
-    H5Tclose( vDatatype  );
     H5Dclose( vDataset   );
 
   }
 
-  //**************************************************************************
 
   void HDF5_ReadVecSize(hid_t group_id, int *size )
   {
     int     rank=1;
     hsize_t dimension = 1;
 
-    hid_t mDataspace = H5Screate_simple(rank, &dimension, NULL);
     hid_t vDataspace = H5Screate_simple(rank, &dimension, NULL);
-    hid_t vDatatype  = H5Tcopy(H5T_NATIVE_INT);
+    hid_t vDatatype  = H5T_NATIVE_INT;
     hid_t vDataset   = H5Dopen( group_id, "VecSize");
-    H5Dread( vDataset, vDatatype, mDataspace, vDataspace, H5P_DEFAULT, size);
 
-    H5Sclose( mDataspace );
+    H5Dread( vDataset, vDatatype, H5S_ALL, vDataspace, H5P_DEFAULT, size);
+
     H5Sclose( vDataspace );
-    H5Tclose( vDatatype  );
     H5Dclose( vDataset   );
 
   }
 
-  //***************************************************************************
 }
