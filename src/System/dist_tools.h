6a1a6683e3bfc05c30e3cf289f96a86e683e297b
Krunal Soni <krunalnsoni@gmail.com>
2004-12-03 03:54:00 +0000

    get_mapping has been modified so that maps of head, body or both can be retrieved.  A function added to expand maps in reverse. (Related to code added for duplication of entities.)

--- src/System/dist_tools.h
+++ src/System/dist_tools.h
@@ -10,11 +10,12 @@ namespace Loci {
   extern bool use_dynamic_scheduling ;
   void get_clone(fact_db &facts, const rule_db &rdb) ;
   void categories(fact_db &facts,std::vector<entitySet> &pvec) ;
-  void get_mappings(const rule_db &rdb, fact_db &facts, std::set<std::vector<variableSet> > &maps, bool only_head = false) ;
+  void get_mappings(const rule_db &rdb, fact_db &facts, std::set<std::vector<variableSet> > &maps, unsigned int rule_part = 0) ;
+  
   entitySet expand_map(entitySet domain, fact_db &facts,
 		       const std::set<std::vector<variableSet> > &maps) ;
   entitySet dist_expand_map(entitySet domain, fact_db &facts,
-		       const std::set<std::vector<variableSet> > &maps) ;
+			    const std::set<std::vector<variableSet> > &maps) ;
   entitySet fill_entitySet(const entitySet& e, fact_db &facts) ;
   std::vector<entitySet> fill_entitySet(const std::vector<entitySet>& e,
                                         fact_db &facts) ;
@@ -23,7 +24,10 @@ namespace Loci {
                                         fact_db &facts) ;
   void metis_facts(fact_db &facts, std::vector<entitySet> &ptn, int num_partitions = 0 ) ;
   entitySet context_for_map_output(entitySet domain, fact_db &facts,
-				  const std::set<std::vector<variableSet> > &maps);
+				   const std::set<std::vector<variableSet> > &maps);
+  entitySet dist_reverse_expand_map(fact_db &facts,
+                                    const std::set<std::vector<variableSet> > &maps);
+  
 }
 
 #endif
