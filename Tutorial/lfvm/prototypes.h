15713db1d61159435d4735ca8fc932df208142ec
cvs2git <>
2005-03-12 23:11:52 +0000

    This commit was manufactured by cvs2git to create tag 'rel-3-beta-0'.

--- Tutorial/lfvm/prototypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROTOTYPES_H
-#define PROTOTYPES_H
-#include <Loci.h>
-#include <istream>
-void read_triangles(std::istream &ifile, fact_db &facts) ;
-void setup_edges(fact_db &facts) ;
-
-  
-#endif
