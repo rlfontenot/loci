50419b882a821041b98d910ce24dbc326c167302
Krunal Soni <krunalnsoni@gmail.com>
2005-04-22 00:21:09 +0000

    More options related to duplication of work are added so that different levels of duplication can be specified.

--- src/System/Initialize.cc
+++ src/System/Initialize.cc
@@ -61,6 +61,8 @@ namespace Loci {
   bool use_old_dependency_graph = false ;
 
   bool duplicate_work = false;
+  bool extra_duplication = false;
+  bool multilevel_duplication = false;
   /////////////////////////////
   
   ofstream debugout ;
@@ -223,6 +225,19 @@ namespace Loci {
         use_old_dependency_graph = true ;
         i++ ;
       } else if(!strcmp((*argv)[i],"--duplicate_work")){
+	duplicate_work = true;
+	i++;
+      } else if(!strcmp((*argv)[i],"--extra_duplication")){
+	extra_duplication = true;
+	duplicate_work = true;
+	i++;
+      } else if(!strcmp((*argv)[i],"--multilevel_duplication")){
+	multilevel_duplication = true;
+	duplicate_work = true;
+	i++;
+      } else if(!strcmp((*argv)[i],"--extreme_duplication")){
+	extra_duplication = true;
+	multilevel_duplication = true;
 	duplicate_work = true;
 	i++;
       }
