5109c7468b8117210d926069354a379e67b6c261
Krunal Soni <krunalnsoni@gmail.com>
2005-07-11 19:06:41 +0000

    Collecting more than one data points for timing of a rule.

--- src/System/execution_factory.cc
+++ src/System/execution_factory.cc
@@ -18,7 +18,7 @@ namespace Loci {
       }
     }
     
-    if(Loci::collect_timings)
+    if(Loci::collect_timings && rule_tag.targets().begin()->get_info().name != "OUTPUT")
       return new timed_execute_rule(rule_tag, exec_seq, facts, scheds,
 				    Loci::time_duration_to_collect_data);
     else
@@ -45,7 +45,7 @@ namespace Loci {
 		     const storeRepP &p, const sched_db &scheds, double td)
     :execute_rule(fi, seq, facts, v, p, scheds), min_time_duration(td){
   }
-  
+
   void timed_execute_rule::execute(fact_db &facts) {
     int count = 0;
     double et;
@@ -56,9 +56,10 @@ namespace Loci {
       et = MPI_Wtime();
       count++;
     }while(et - st < min_time_duration); 
-    
-    timeout << rule_tag <<"\t" << count << "\t" << et-st << "\t";
-    
+
+    //rule_tag.PrintName(timeout);
+    timeout << rule_tag.get_info().name();
+    timeout << "\t" << count << "\t" << et-st << "\t";
     count = 0;
     st = MPI_Wtime();
     do{
@@ -67,6 +68,84 @@ namespace Loci {
       count++;
     }while(et - st < min_time_duration);
       
-    timeout << exec_seq.size() <<"\t" << count << "\t" << et-st << endl;
+    timeout << exec_seq.size() <<"\t" << count << "\t" << et-st << "\t";
+    
+    std::map<variable, std::pair<int, unsigned char *> > storeMap;
+    variableSet targets = rule_tag.targets();
+    for(variableSet::const_iterator vi = targets.begin(); 
+	vi != targets.end(); vi++) {
+      storeRepP vRep = facts.get_variable(*vi);
+      int size = vRep->pack_size(vRep->domain());
+      unsigned char * buf = new unsigned char[size];
+      int position = 0;
+      vRep->pack(buf, position, size, vRep->domain());
+      std::pair<int, unsigned char *> myPair(size, buf);
+      storeMap[*vi] = myPair;
+    }
+    
+    int size_factor[4];
+    if(exec_seq.size() <= 100) {
+      size_factor[0] = 2;
+      size_factor[1] = 4;
+      size_factor[2] = 8;
+      size_factor[3] = 16;
+    }
+    else if(exec_seq.size() <= 1000) {
+      size_factor[0] = -2;
+      size_factor[1] = 2;
+      size_factor[2] = 4;
+      size_factor[3] = 8;
+    }
+    else if(exec_seq.size() <= 10000) {
+      size_factor[0] = -4;
+      size_factor[1] = -2;
+      size_factor[2] = 2;
+      size_factor[3] = 4;
+    }
+    else if(exec_seq.size() <= 50000) {
+      size_factor[0] = -8;
+      size_factor[1] = -4;
+      size_factor[2] = -2;
+      size_factor[3] = 2;
+    }
+    else {
+      size_factor[0] = -16;
+      size_factor[1] = -8;
+      size_factor[2] = -4;
+      size_factor[3] = -2;
+    }
+
+    for(int i = 0; i < 4; i++) {
+      sequence my_seq;
+      if(size_factor[i] < 0) {
+	int size = exec_seq.size()/size_factor[i]*(-1);
+	sequence::const_iterator si = exec_seq.begin();
+	for(int j = 0; j < size; j++) {
+	  my_seq += *si;
+	  si++;
+	}
+      }
+      else {
+	for(int j = 0; j < size_factor[i]; j++) {
+	  my_seq += exec_seq;
+	}
+      }
+      count = 0;
+      st = MPI_Wtime();
+      do{
+	rp->compute(my_seq);
+	et = MPI_Wtime();
+	count++;
+      }while(et - st < min_time_duration);
+      timeout << my_seq.size() <<"\t" << count << "\t" << et-st << "\t";
+    }
+    timeout << endl;
+    for(std::map<variable, std::pair<int, unsigned char*> >::iterator mi = storeMap.begin();
+	mi != storeMap.end(); mi++) {
+      storeRepP vRep = facts.get_variable(mi->first);
+      int position = 0;
+      vRep->unpack(mi->second.second, position, mi->second.first, vRep->domain());
+      delete [] mi->second.second;
+    }
   }
 }
