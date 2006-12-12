b60f69d026664b782f3bf867bf494ad09ac012ab
Ed Luke <edluke@gmail.com>
2006-12-12 17:15:20 +0000

    Added parmetis to the source distribution

--- src/Install.bash
+++ src/Install.bash
@@ -13,6 +13,9 @@ echo Installing Library Files
 cp Tools/libTools.so $INSTALL_PATH/lib
 cp System/libLoci.so $INSTALL_PATH/lib
 cp FVMMod/fvm_m.so $INSTALL_PATH/lib
+cp ParMetis-3.1/METISLib/libmetis.so $INSTALL_PATH/lib
+cp ParMetis-3.1/ParMETISLib/libparmetis.so $INSTALL_PATH/lib
+
 
 echo Installing Loci Tools
 cp lpp/lpp $INSTALL_PATH/bin
