57b297e841126bc0c3ce31a95f990a1cf9aa0a02
Ed Luke <edluke@gmail.com>
2007-11-15 21:28:45 +0000

    Updated to support volume grid and general affine transforms.

--- FVMtools/vogmerge.cc
+++ FVMtools/vogmerge.cc
@@ -14,6 +14,7 @@ using std::string ;
 using std::cout ;
 using std::cerr ;
 using std::endl ;
+using Loci::vector3d ;
 
 struct file_info {
   unsigned long numNodes ;
@@ -30,6 +31,99 @@ unsigned long readAttributeLong(hid_t group, char *name) {
   return val ;
 }
 
+struct affineMapping {
+  double M[4][4] ;
+  affineMapping() {
+    for(int i=0;i<4;++i) {
+      for(int j=0;j<4;++j)
+        M[i][j] = 0 ;
+      M[i][i] = 1 ;
+    }
+  }
+  void Combine(affineMapping a) {
+    double Mtmp[4][4] ;
+    for(int i=0;i<4;++i)
+      for(int j=0;j<4;++j)
+        Mtmp[i][j] = 0 ;
+    for(int i=0;i<4;++i)
+      for(int j=0;j<4;++j) {
+        double mtmp = 0 ;
+        for(int k=0;k<4;++k)
+          mtmp += a.M[i][k]*M[k][j] ;
+        Mtmp[i][j] = mtmp ;
+      }
+    for(int i=0;i<4;++i)
+      for(int j=0;j<4;++j)
+        M[i][j] = Mtmp[i][j] ;
+  }
+  void translate(vector3d<double> tv) {
+    affineMapping tmp ;
+    tmp.M[0][3] = tv.x ;
+    tmp.M[1][3] = tv.y ;
+    tmp.M[2][3] = tv.z ;
+    Combine(tmp) ;
+  }
+  void scale(vector3d<double> tv) {
+    affineMapping tmp ;
+    tmp.M[0][0] = tv.x ;
+    tmp.M[1][1] = tv.y ;
+    tmp.M[2][2] = tv.z ;
+    Combine(tmp) ;
+  }
+  void rotateX(double theta) {
+    double th = theta*2.*M_PI/360. ;
+    double sth = sin(th) ;
+    double cth = cos(th) ;
+    affineMapping tmp ;
+
+    tmp.M[1][1] =  cth ;
+    tmp.M[1][2] =  sth ;
+    tmp.M[2][1] = -sth ;
+    tmp.M[2][2] =  cth ;
+    Combine(tmp) ;
+  }
+  void rotateY(double theta) {
+    double th = theta*2.*M_PI/360. ;
+    double sth = sin(th) ;
+    double cth = cos(th) ;
+    affineMapping tmp ;
+
+    tmp.M[0][0] =  cth ;
+    tmp.M[0][2] = -sth ;
+    tmp.M[2][0] =  sth ;
+    tmp.M[2][2] =  cth ;
+    Combine(tmp) ;
+  }
+  void rotateZ(double theta) {
+    double th = theta*2.*M_PI/360. ;
+    double sth = sin(th) ;
+    double cth = cos(th) ;
+    affineMapping tmp ;
+
+    tmp.M[0][0] =  cth ;
+    tmp.M[0][1] =  sth ;
+    tmp.M[1][0] = -sth ;
+    tmp.M[1][1] =  cth ;
+    Combine(tmp) ;
+  }
+  vector3d<double> Map(vector3d<double> v) {
+    double tmp[4] ;
+    tmp[0] = v.x ;
+    tmp[1] = v.y ;
+    tmp[2] = v.z ;
+    tmp[3] = 1. ;
+    double res[4] ;
+    for(int i=0;i<4;++i)
+      res[i] = 0 ;
+    for(int i=0;i<4;++i)
+      for(int j=0;j<4;++j)
+        res[i] += M[i][j]*tmp[j] ;
+    vector3d<double> r(res[0],res[1],res[2]) ;
+    return r ;
+  }
+   
+} ;
+
 namespace Loci {
   extern int getClusterNumFaces(unsigned char *cluster) ;
   extern int fillClusterFaceSizes(unsigned char *cluster, int *sizes) ;
@@ -43,64 +137,255 @@ namespace Loci {
                       entitySet cellSet) ;
 }
 
+
+bool readVolTags(hid_t input_fid,
+                 vector<pair<string,Loci::entitySet> > &volDat) {
+  using namespace Loci ;
+  /* Save old error handler */
+  herr_t (*old_func)(void*) = 0;
+  void *old_client_data = 0 ;
+  H5Eget_auto(&old_func, &old_client_data);
+  /* Turn off error handling */
+  H5Eset_auto(NULL, NULL);
+
+  vector<pair<string,entitySet> > volTags ;
+  hid_t cell_info = H5Gopen(input_fid,"cell_info") ;
+  if(cell_info > 0) {
+    vector<string> vol_tag ;
+    vector<entitySet> vol_set ;
+    vector<int> vol_id ;
+    
+    hsize_t num_tags = 0 ;
+    H5Gget_num_objs(cell_info,&num_tags) ;
+    for(hsize_t tg=0;tg<num_tags;++tg) {
+      char buf[1024] ;
+      memset(buf, '\0', 1024) ;
+      H5Gget_objname_by_idx(cell_info,tg,buf,sizeof(buf)) ;
+      buf[1023]='\0' ;
+      
+      string name = string(buf) ;
+      hid_t vt_g = H5Gopen(cell_info,buf) ;
+      hid_t id_a = H5Aopen_name(vt_g,"Ident") ;
+      int ident ;
+      H5Aread(id_a,H5T_NATIVE_INT,&ident) ;
+      H5Aclose(id_a) ;
+      entitySet dom ;
+      HDF5_ReadDomain(vt_g,dom) ;
+      vol_tag.push_back(name) ;
+      vol_set.push_back(dom) ;
+      vol_id.push_back(ident) ;
+      H5Gclose(vt_g) ;
+    }
+    int maxi =0 ;
+    for(size_t i=0;i<vol_id.size();++i)
+      maxi = max(vol_id[i],maxi) ;
+    vector<pair<string,entitySet> > tmp(maxi+1) ;
+    volTags.swap(tmp) ;
+    for(size_t i=0;i<vol_id.size();++i) {
+      volTags[vol_id[i]].first = vol_tag[i] ;
+      volTags[vol_id[i]].second = vol_set[i] ;
+    }
+  } else {
+    hid_t file_info = H5Gopen(input_fid,"file_info") ;
+    long numCells = readAttributeLong(file_info,"numCells") ;
+    volTags.push_back(pair<string,entitySet>
+                     (string("Main"),
+                      entitySet(interval(0,numCells-1)))) ;
+    H5Gclose(file_info) ;
+  }
+  
+  /* Restore previous error handler */
+  H5Eset_auto(old_func, old_client_data);
+  volDat.swap(volTags) ;
+  return true ;
+}
+
+void Usage() {
+  cout << "Utility for merging vog grids into a single vog file." << endl
+       << endl ;
+  cout << "Usage: " << endl
+       << "  vogmerge <options>" << endl
+       << "where <options> may be given by:" << endl 
+       <<  endl ;
+  cout << "  -g <gridfile>           | Specify input grid filename" << endl
+       << "  -o <gridfile>           | Specify output grid filename" << endl
+       << "  -xshift <value>         | translate grid in x-coordinate" << endl
+       << "  -yshift <value>         | translate grid in y-coordinate" << endl
+       << "  -zshift <value>         | translate grid in z-coordinate" << endl
+       << "  -xscale <value>         | scale grid in x-coordinate" << endl
+       << "  -yscale <value>         | scale grid in y-coordinate" << endl
+       << "  -zscale <value>         | scale grid in z-coordinate" << endl
+       << "  -scale  <value>         | scale grid in all coordinates" << endl
+       << "  -xrotate <value>        | rotate scale about x-axis (degrees)" << endl
+       << "  -yrotate <value>        | rotate scale about y-axis (degrees)" << endl
+       << "  -zrotate <value>        | rotate scale about z-axis (degrees)" << endl
+       << "  -bc <oldname>,<newname> | rename boundary surface" << endl
+       << "  -tag <name>             | specify volume tag for input grid" << endl
+       << endl ;
+}
+  
 int main(int ac, char *av[]) {
   using Loci::entitySet ;
   using Loci::vector3d ;
   Loci::Init(&ac, &av) ;
-  string output_file = "out" ;
+  string output_file = "merged.vog" ;
   vector<string> input_files ;
   vector<map<string,string> > bc_rename ;
-  vector<vector3d<double> > translate ;
+  vector<vector<pair<string,entitySet> > > volTag ;
+  vector<bool> applyMap ;
+  vector<affineMapping> gridXform ;
   for(int i=1;i<ac;++i) {
     string opt = av[i] ;
+    bool parsed = false ;
     if(opt == "-g") {
       i++ ;
       if(i<ac) {
         input_files.push_back(string(av[i])) ;
-        translate.push_back(vector3d<double>(0,0,0)) ;
         bc_rename.push_back(map<string,string>()) ;
+        volTag.push_back(vector<pair<string,entitySet> >()) ;
+        applyMap.push_back(false) ;
+        gridXform.push_back(affineMapping()) ;
+        parsed = true ;
       }
     }
     if(input_files.size() > 0) {
       if(opt == "-xshift") {
         i++ ;
-        if(i<ac)
-          translate.back().x += atof(av[i]) ;
+        if(i<ac) {
+          vector3d<double> tv(atof(av[i]),0,0) ;
+          gridXform.back().translate(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
       }
       if(opt == "-yshift") {
         i++ ;
-        if(i<ac)
-          translate.back().y += atof(av[i]) ;
+        if(i<ac) {
+          vector3d<double> tv(0,atof(av[i]),0) ;
+          gridXform.back().translate(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
       }
       if(opt == "-zshift") {
         i++ ;
-        if(i<ac)
-          translate.back().z += atof(av[i]) ;
+        if(i<ac) {
+          vector3d<double> tv(0,0,atof(av[i])) ;
+          gridXform.back().translate(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
       }
+      if(opt == "-xrotate") {
+        i++ ;
+        if(i<ac) {
+          gridXform.back().rotateX(atof(av[i])) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }
+      if(opt == "-yrotate") {
+        i++ ;
+        if(i<ac) {
+          gridXform.back().rotateY(atof(av[i])) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }
+      if(opt == "-zrotate") {
+        i++ ;
+        if(i<ac) {
+          gridXform.back().rotateZ(atof(av[i])) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }
+      if(opt == "-xscale") {
+        i++ ;
+        if(i<ac) {
+          vector3d<double> tv(atof(av[i]),1.,1.) ;
+          gridXform.back().scale(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }
+      if(opt == "-yscale") {
+        i++ ;
+        if(i<ac) {
+          vector3d<double> tv(1.,atof(av[i]),1.) ;
+          gridXform.back().scale(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }
+      if(opt == "-zscale") {
+        i++ ;
+        if(i<ac) {
+          vector3d<double> tv(1.,1.,atof(av[i])) ;
+          gridXform.back().scale(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }
+      if(opt == "-scale") {
+        i++ ;
+        if(i<ac) {
+          double v = atof(av[i]) ;
+          vector3d<double> tv(v,v,v) ;
+          gridXform.back().scale(tv) ;
+          applyMap.back() = true ;
+          parsed = true ;
+        }
+      }      
+      
       if(opt== "-bc") {
         i++ ;
         if(i<ac) {
           char *p = index(av[i],',') ;
           if(p==0) {
             cerr << "-bc option, comma missing in argument" << endl ;
+            Usage() ;
             exit(-1) ;
           }
           *p = '\0' ;
           
           string first(av[i]), second(p+1) ;
           bc_rename.back()[first]=second ;
+          parsed = true ;
+        }
+      }
+      if(opt=="-tag") {
+        i++ ;
+        if(i<ac) {
+          vector<pair<string,entitySet> > vp ;
+          vp.push_back(pair<string,entitySet>(string(av[i]),Loci::EMPTY)) ;
+          volTag.back() = vp ;
+          parsed = true ;
         }
       }
     }
         
     if(opt == "-o") {
       i++ ;
-      if(i<ac)
+      if(i<ac) {
         output_file = string(av[i]) ;
+        parsed = true ;
+      }
+    }
+
+    if(!parsed) {
+      cerr << "unable to parse command line argument '" << av[i] << "'" << endl ;
+      Usage() ;
+      exit(-1) ;
     }
   }
 
   int num_inputs = input_files.size() ;
+  if(num_inputs == 0) {
+    cerr << "no input grid files to process!" << endl ;
+    Usage() ;
+    exit(-1) ;
+  }
   vector<hid_t> input_fid(num_inputs) ;
 
   bool fail = false ;
@@ -142,8 +427,22 @@ int main(int ac, char *av[]) {
     
     H5Gclose(fi) ;
   }
-  
 
+  // Setup volume tags
+  for(int i=0;i<num_inputs;++i) {
+    entitySet compSet = Loci::interval(0,fileData[i].numCells-1) ;
+    if(volTag[i].size() == 0) {
+      vector<pair<string,entitySet> > volDat ;
+      readVolTags(input_fid[i],volDat) ;
+      for(size_t j=0;j<volDat.size();++j)
+        volTag[i].push_back(volDat[j]) ;
+    } else {
+      if(volTag[i].size() != 1)
+        cerr << "Grid '" << input_files[i] << "' has more than one volume tag!" << endl ;
+      volTag[i][0].second = compSet ;
+    }
+  }
+  
   
   vector<vector<pair<int,string> > > boundary_ids(num_inputs) ;
   for(int i=0;i<num_inputs;++i) {
@@ -176,6 +475,7 @@ int main(int ac, char *av[]) {
     numFaces += fileData[i].numFaces ;
     numCells += fileData[i].numCells ;
   }
+
 
   // Begin writing output file
 
@@ -249,9 +549,10 @@ int main(int ac, char *av[]) {
       H5Dclose(dataset) ;
       H5Gclose(node_g) ;
 
-      for(size_t ii=0;ii<pos_dat.size();++ii)
-        pos_dat[ii] += translate[i] ;
-      
+      if(applyMap[i]) {
+        for(size_t ii=0;ii<pos_dat.size();++ii)
+          pos_dat[ii] = gridXform[i].Map(pos_dat[ii]) ;
+      }
       // Now write out this file's part of the positions
       H5Sselect_hyperslab(dataspace,H5S_SELECT_SET, &start,&stride,&count,NULL)  ;
       dimension = count ;
@@ -497,6 +798,50 @@ int main(int ac, char *av[]) {
     H5Gclose(bc_id) ;
   }
   H5Gclose(surf_id) ;
+
+
+  // Compute Volume Tags
+  map<string,entitySet> volTagMap ;
+  int cellcnt = 0 ;
+  for(int i=0;i<num_inputs;++i) {
+    for(size_t j=0;j<volTag[i].size();++j) {
+      string tag = volTag[i][j].first ;
+      entitySet tagSet = (volTag[i][j].second >> cellcnt) ;
+      volTagMap[tag] += tagSet ;
+    }
+    cellcnt+= fileData[i].numCells ;
+  }
+  vector<pair<string,entitySet> > volTags ;
+
+  volTags.push_back(pair<string,entitySet>(string("Main"),volTagMap["Main"])) ;
+                    
+  map<string,entitySet>::const_iterator vtmi ;
+  for(vtmi=volTagMap.begin();vtmi!=volTagMap.end();++vtmi) {
+    if(vtmi->first != "Main")
+      volTags.push_back(pair<string,entitySet>(vtmi->first,vtmi->second)) ;
+  }
+
+  cout << "volume tags =" << endl  ;
+  for(size_t i=0;i<volTags.size();++i)
+    cout << i << " - " << volTags[i].first << volTags[i].second << endl ;
+
+  
+  hid_t cell_info = H5Gcreate(output_fid,"cell_info", 0) ;
+
+  for(size_t i=0;i<volTags.size();++i) {
+    hid_t vol_id = H5Gcreate(cell_info,volTags[i].first.c_str(),0) ;
+    hsize_t dims = 1 ;
+    hid_t dataspace_id = H5Screate_simple(1,&dims,NULL) ;
+    
+    hid_t att_id = H5Acreate(vol_id,"Ident", H5T_NATIVE_INT,
+                             dataspace_id, H5P_DEFAULT) ;
+    int num = int(i) ;
+    H5Awrite(att_id,H5T_NATIVE_INT,&num) ;
+    H5Aclose(att_id) ;
+    Loci::HDF5_WriteDomain(vol_id,volTags[i].second) ;
+    H5Gclose(vol_id) ;
+  }
+  H5Gclose(cell_info) ;
   
   // Close everything up
   for(int i=0;i<num_inputs;++i) 
