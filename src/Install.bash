902b08e5dd7cdeb701ab55d5af80534b67f9d56a
Ed Luke <edluke@gmail.com>
2007-12-02 17:07:41 +0000

    Added code to install vogcheck and vogmerge

--- src/Install.bash
+++ src/Install.bash
@@ -29,6 +29,8 @@ cp FVMtools/xdr2vog $INSTALL_PATH/bin
 cp FVMtools/vog2xdr $INSTALL_PATH/bin
 cp FVMtools/ugrid2vog $INSTALL_PATH/bin
 cp FVMtools/cfd++2xdr $INSTALL_PATH/bin
+cp FVMtools/vogmerge $INSTALL_PATH/bin
+cp FVMtools/vogcheck $INSTALL_PATH/bin
 
 echo cp Loci.conf comp.conf sys.conf $INSTALL_PATH
 cp Loci.conf comp.conf sys.conf $INSTALL_PATH
