c88fe61e7cce0a2016c3823fc36566843e9ce159
Ed Luke <edluke@gmail.com>
2011-02-21 23:11:38 +0000

    Update to eliminate debug output unless verbose is selected

--- src/System/sched_comp.cc
+++ src/System/sched_comp.cc
@@ -917,9 +917,9 @@ namespace Loci {
         }
 	if(srp->RepType() == Loci::STORE) {
 	  entitySet tmp = interval(alloc_dom.Min(), alloc_dom.Max()) ;
-	  if(tmp.size() >= 2*srp->domain().size())
+	  if(verbose && tmp.size() >= 2*srp->domain().size())
 	    Loci::debugout << "Variable = " << *vi << "  more than twice the space allocated :  allocated over " << alloc_dom << " size = " << tmp.size()  << "  while domain is only  " << srp->domain() << " size = " << srp->domain().size() << endl ;
-	  if(alloc_dom != srp->domain()) {
+	  if(verbose && alloc_dom != srp->domain()) {
 	    Loci::debugout << "reallocating " << *vi << "  over  " << alloc_dom << " initially it was over  " << srp->domain() << endl ;
 	    srp->allocate(alloc_dom) ;
 	  }
