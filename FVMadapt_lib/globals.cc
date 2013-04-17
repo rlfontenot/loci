702e4bf57d0a01675a1bdecc600c2b96338a1e2a
Ed Luke <edluke@gmail.com>
2013-04-17 01:12:27 +0000

    Added new database for in-core file transfer.

--- FVMadapt_lib/globals.cc
+++ FVMadapt_lib/globals.cc
@@ -19,6 +19,7 @@
 //#
 //#############################################################################
 #include "globals.h"
+#include "dataxferDB.h"
 
 double Globals::tolerance = 1e-10;
 double  Globals::fold = 90.0*3.1415926/180.0; //90 degree
@@ -31,3 +32,7 @@ int Globals::factor = 2;
 int Globals::balance_option = 0;
 vect3d Globals::split = vect3d(0.0, 0.0, 1.0);
 vect3d Globals::nosplit = vect3d(0.0, 0.0, 1.0);
+
+namespace Loci {
+  DataXFERDatabase DataXFER_DB ;
+}
