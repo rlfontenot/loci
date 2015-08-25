8d00c115e56bd6b430994e7e6110ae809588313d
Ed Luke <edluke@gmail.com>
2015-08-25 19:18:43 +0000

    Updated copyright message

--- src/FVMAdapt/build_prismcell.cc
+++ src/FVMAdapt/build_prismcell.cc
@@ -1,3 +1,23 @@
+//#############################################################################
+//#
+//# Copyright 2015, Mississippi State University
+//#
+//# This file is part of the Loci Framework.
+//#
+//# The Loci Framework is free software: you can redistribute it and/or modify
+//# it under the terms of the Lesser GNU General Public License as published by
+//# the Free Software Foundation, either version 3 of the License, or
+//# (at your option) any later version.
+//#
+//# The Loci Framework is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# Lesser GNU General Public License for more details.
+//#
+//# You should have received a copy of the Lesser GNU General Public License
+//# along with the Loci Framework.  If not, see <http://www.gnu.org/licenses>
+//#
+//#############################################################################
 // This file contain funstions that build a prism according to Loci data structures
 // and refinement plans. When build a prism, edges are built as local edges in cell
 // and resplit with needReverse. both general faces and quadfaces faces are built as
