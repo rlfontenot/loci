ba0847b0f4004a420cff52293939840873e91e5e
Ed Luke <edluke@gmail.com>
2017-07-06 05:26:11 +0000

    update to silence compiler warnings when using -DDEBUG

--- src/System/visit_chomp.cc
+++ src/System/visit_chomp.cc
@@ -1140,8 +1140,8 @@ namespace Loci {
           }
         }
         
-        WARN(reduction && pointwise || pointwise && singleton ||
-             reduction && singleton) ;
+        WARN((reduction && pointwise) || (pointwise && singleton) ||
+             (reduction && singleton)) ;
 
         if((use_rules != EMPTY)) {
           if( (priority_rule || pointwise) &&
