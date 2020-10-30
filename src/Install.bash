3542ed79f0be0b3e116c838e9464d59ec82f0e96
Ed Luke <edluke@gmail.com>
2020-10-30 20:04:42 +0000

    Adding cfd++2vog converter

--- src/Install.bash
+++ src/Install.bash
@@ -66,6 +66,7 @@ cp FVMtools/make_periodic $INSTALL_PATH/bin
 cp FVMtools/plot3d2vog $INSTALL_PATH/bin
 cp FVMtools/vog2surf $INSTALL_PATH/bin
 cp FVMtools/ugrid2vog $INSTALL_PATH/bin
+cp FVMtools/cfd++2vog $INSTALL_PATH/bin
 cp FVMtools/fluent2vog $INSTALL_PATH/bin
 cp FVMtools/ccm2vog $INSTALL_PATH/bin
 cp FVMtools/vogmerge $INSTALL_PATH/bin
