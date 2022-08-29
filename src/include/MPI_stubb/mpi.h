bed16e91d357bf126f523d7d841b31177da55f38
Ed Luke <edluke@gmail.com>
2022-08-29 15:45:40 +0000

    Update to add mpi IO routines to mpi stubbs for nompi compile support

--- src/include/MPI_stubb/mpi.h
+++ src/include/MPI_stubb/mpi.h
@@ -816,6 +816,37 @@ enum {
 
  int  MPI_Finalized(int *);
 
+
+  // IO
+  typedef int MPI_File ;
+  typedef int MPI_Offset ;
+
+  enum {
+	MPI_MODE_WRONLY                 = 1,
+	MPI_MODE_RDONLY                 = 2,
+	MPI_MODE_CREATE                 = 4,
+	
+	
+  };
+
+  int MPI_File_open(MPI_Comm comm, const char *filename, int amode, MPI_Info info,
+		    MPI_File *fh) ;
+  int MPI_File_close(MPI_File *fh) ;
+  int MPI_File_read(MPI_File fh, void *buf, int count, MPI_Datatype datatype,
+		    MPI_Status *status) ;
+  int MPI_File_read_at_all(MPI_File fh, MPI_Offset offset, void *buf,
+			   int count, MPI_Datatype datatype, 
+			   MPI_Status *status)  ;
+  int MPI_File_read_at(MPI_File fh, MPI_Offset offset, void *buf,
+		       int count, MPI_Datatype datatype, MPI_Status *status) ;
+  int MPI_File_write(MPI_File fh, const void *buf, int count,
+		     MPI_Datatype datatype, MPI_Status *status) ;
+  int MPI_File_write_at(MPI_File fh, MPI_Offset offset, const void *buf,
+			int count, MPI_Datatype datatype, MPI_Status *status) ;
+  int MPI_File_write_at_all(MPI_File fh, MPI_Offset offset, const void *buf,
+			    int count, MPI_Datatype datatype, 
+			    MPI_Status *status) ;
+
 #ifdef __cplusplus
 }
 #endif
