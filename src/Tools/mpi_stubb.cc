d27e7dca8ed323eca2e60e52ab0cdfa207df92f1
Ed Luke <edluke@gmail.com>
2022-09-22 04:26:46 +0000

    Update to update MPI stubs with new MPI functions

--- src/Tools/mpi_stubb.cc
+++ src/Tools/mpi_stubb.cc
@@ -1448,5 +1448,64 @@ extern "C" {
     return 0 ;
   }
 
+  // MPI_IO
+  int MPI_File_open(MPI_Comm comm, const char *filename, int amode,
+		    MPI_Info info, MPI_File *fh)
+  {
+    err_report();
+    return 0;
+  }
+
+  int MPI_File_close(MPI_File *fh)
+  {
+    err_report();
+    return 0;
+  }
+    
+  int MPI_File_read(MPI_File fh, void *buf, int count, MPI_Datatype datatype,
+		    MPI_Status *status)
+  {
+    err_report();
+    return 0;
+  }
+    
+  int MPI_File_read_at_all(MPI_File fh, MPI_Offset offset, void *buf,
+			   int count, MPI_Datatype datatype, 
+			   MPI_Status *status)
+  {
+    err_report();
+    return 0;
+  }
+    
+  int MPI_File_read_at(MPI_File fh, MPI_Offset offset, void *buf,
+		       int count, MPI_Datatype datatype, MPI_Status *status)
+  {
+    err_report();
+    return 0;
+  }
+    
+  int MPI_File_write(MPI_File fh, const void *buf, int count,
+		     MPI_Datatype datatype, MPI_Status *status)
+  {
+    err_report();
+    return 0;
+  }
+    
+  int MPI_File_write_at(MPI_File fh, MPI_Offset offset, const void *buf,
+			int count, MPI_Datatype datatype, MPI_Status *status)
+  {
+    err_report();
+    return 0;
+  }
+    
+  int MPI_File_write_at_all(MPI_File fh, MPI_Offset offset, const void *buf,
+			    int count, MPI_Datatype datatype, 
+			    MPI_Status *status)
+  {
+    err_report();
+    return 0;
+  }
+    
+  
 }
 #endif
