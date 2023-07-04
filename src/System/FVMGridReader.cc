7590f761947b82b2360d5061f31809c700174c00
Ed Luke <edluke@gmail.com>
2023-07-04 20:31:51 +0000

    Fixed weights file input to remove space in variable name

--- src/System/FVMGridReader.cc
+++ src/System/FVMGridReader.cc
@@ -2746,7 +2746,7 @@ namespace Loci {
 	  // read
 	   
 	  
-	  readContainerRAW(file_id,"cell weight", cell_weights.Rep(),
+	  readContainerRAW(file_id,"cellweight", cell_weights.Rep(),
 			   MPI_COMM_WORLD) ;
 	  Loci::hdf5CloseFile(file_id) ;
 	} else if(cell_weight_store != 0){
@@ -2831,7 +2831,7 @@ namespace Loci {
 	    // read
 	   
 	    
-	    readContainerRAW(file_id,"cell weight", cell_weights.Rep(),
+	    readContainerRAW(file_id,"cellweight", cell_weights.Rep(),
 			     MPI_COMM_WORLD) ;
             Loci::hdf5CloseFile(file_id) ;
           }else if(cell_weight_store != 0){
